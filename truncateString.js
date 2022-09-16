8d30a9409ec96171dd02ea9e20b1b23d137d0e7c
lanefoxwood <lanefoxwood@gmail.com>
2022-09-16 15:55:55 +0200

    added logic comments

--- truncateString.js
+++ truncateString.js
@@ -15,3 +15,42 @@ Truncate a string (first argument) if it is longer than the given maximum string
 1.
 2.append the "..." ending to the end.
 */
+//add comments to demonstrate logic process
+
+//This function should accept a string and a number, and return a string which has been cut off at the specified character. also, the returned string has ellipses "..." added to the end. 
+
+//TODO: I think i can use the slice() method to return part of a string...
+
+//TODO: i need to define this function which accepts the two arguments and returns a string:
+
+function truncateString(string, number){
+    //TODO: inside this function i need to do the following:
+    //TODO: 1. make a copy of the string, and call it stringCopy
+    //TODO: 2. slice from (0, number) and save it to stringCopy
+    //TODO: 3. concat() the ellipses "..." to the end of stringCopy, saving this new value as truncatedString
+    //TODO: 4. return truncatedString as the result of running this function. 
+};
+
+
+
+//=============================================================================
+
+// but how would you even do what the slice() method does?
+
+// at first glance, it seems like what it does is just loop through each index of a string and concat or add it to a placeholder variable.... i would describe it in the case of the following example:
+
+// given the string "happy times" and the number 7, slice it. 
+// well I can count starting at 0 and up to but not including 7...
+// "happy t" is what we should slice out because it's starting at the index of 0, "h" and looping through concatting each index, up to but not including 7 - the 6th index, "t". 
+
+// the placeholder string should be initialized as empty to start, ''.
+// let placeholderString = ''
+//
+// then, a for loop would look like this to start, ending at i < number...
+/* 
+    for(let i = 0; i < 7; i++){
+        placeholderString = placeholderString + string[i]
+    }
+*/
+
+//this ensures that on the first iteration of the loop that empty string "" gets the first index, string[0], concatted onto it, reassigning the value of placeholderString from "" to "h". 
--- whereBelong.js
+++ whereBelong.js
@@ -40,3 +40,42 @@ given an array of numbers [2, 5, 3], sort from lowest number to highest, lowest 
 2. if greater, swap the two.
 3. if less, move to the next value.
 */
+
+//add commnents to demonstrate logic process 
+// this program should accept an array and a number value. it should return a number.
+// the objective of the program is to return the lowest index at which the number value should be inserted into the array, after the array has been sorted from lowest to highest. the returned value should be a number, the index...
+
+
+/// OOOOkay so there are two major parts : sorting the array from lowest to highest, and comparing the number given to each index in the sorted array. first we sort, then we compare. This is more compicated than other problems, but I'm ready to try!
+
+
+// TODO: i need a function to be defined:
+function whereDoIBelong(array, number){
+    // inside the function, I need to do the following:
+    //TODO: 0. make a copy of the array and push the given number onto the end of it. call it arrayCopy.
+    //TODO: 1. sort the arrayCopy from lowest to highest. lowest being at array[0], highest at array[array.length-1].
+        //ok honestly i know that sorting this array of numbers seems daunting, but i think i need some loop to help and ill do an example below.
+            //inside of a loop i need to do the following:
+            //TODO: 1. calculate the min value of the array
+            //TODO: 2. slice it out and push it into the arrayCopy
+            //TODO: repeat until done. 
+    //TODO: 2. get the indexOf(number), save it in a variable called numberIndex
+    //TODO: 3. return (numberIndex - 1)
+};
+
+
+
+//just had an idea as im writing the steps. what if you just push the number into the array, sort it, then return the indexOf(number)-1... i just want the index of the element directly preceeding the number that was pushed in. just return thast index. 
+
+
+
+//=======================================================================================
+
+// how would you sort this list of numbers from lowest to highest? [4, 3, 15, 12, 7]
+
+// read the entire list once
+// create an empty sortedArray to put values that have been sorted into
+// calculate the maximum value of the list, push it onto the end of the sortedArray.
+//calculate the minimum value of the list, shift it onto the beginning of the sortedArray.
+// ....... so far, the sortedArray looks like this: [3,15]
+//how would you choose the next number to take?  
